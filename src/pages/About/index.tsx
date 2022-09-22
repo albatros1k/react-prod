32a97c5b09d67a3aa3a77cc96472db5a960776d2
Albert <superalbertem@gmail.com>
2022-09-22 14:37:58 +0200

    Cash

--- src/pages/About/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from "react-i18next";
-
-const About = () => {
-  const { t } = useTranslation("about");
-
-  return <div>{t("About")}</div>;
-};
-
-export default About;
--- src/pages/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from "react-i18next";
-
-const Main = () => {
-  const { t } = useTranslation("main");
-
-  return <div>{t("Main")}</div>;
-};
-
-export default Main;
